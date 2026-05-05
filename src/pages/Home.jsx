387d69576abbc1608b1e2165a4543cfbcb404e6d
Rajesh Biswas <maxjoy146@gmail.com>
2026-05-06 01:28:34 +0530

    fix latest updates import path

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { pyqData } from "../data/pyqData";
-import LatestUpdates from "../components/LatestUpdates";
+import LatestUpdates from "../components/LatestUpdates.jsx";
 import Footer from "../components/Footer";
 
 function Home() {
