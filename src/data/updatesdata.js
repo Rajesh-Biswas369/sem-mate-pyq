454bf6f7e0208075c949c676edc37bdd7ce79f0f
Rajesh Biswas <maxjoy146@gmail.com>
2026-05-13 03:33:12 +0530

    updates

--- src/data/updatesdata.js
+++ src/data/updatesdata.js
@@ -1,23 +1,18 @@
 export const updatesData = [
+
   {
-    title: "SSM Folders Updated ✅",
+    title: "Electrical Instrumentation updated",
     description:
-      "Semester 4 → Sequential Systems & Microprocessor now has Study Materials, PYQs, and Solutions folders.",
-    tag: "Updated",
-    date: "Latest",
+      "Instrumentation materials, PYQs, and solutions have been added.",
+    tag: "Live",
+    date: "13-05-2026",
   },
+  
   {
-    title: "SSM Materials Subfolders Added 📚",
+    title: "Upcoming : Power Supply Systems",
     description:
-      "Study Materials now includes Microprocessor and Sequential System folders with phase-wise documents and roadmaps.",
-    tag: "New",
-    date: "Just Now",
-  },
-  {
-    title: "Upcoming Subject: Instrumentation",
-    description:
-      "Instrumentation materials, PYQs, and solutions will be added next.",
+      "Power Supply Systems materials, PYQs, and solutions will be added soon.",
     tag: "Coming Soon",
-    date: "Soon",
+    date: "soon",
   },
 ];
