5544ecdf1d8ad8eabf9663389281b0b72d8661c6
Rajesh Biswas <maxjoy146@gmail.com>
2026-05-16 18:17:43 +0530

    added PSS

--- src/data/updatesdata.js
+++ src/data/updatesdata.js
@@ -1,17 +1,17 @@
 export const updatesData = [
 
   {
-    title: "Electrical Instrumentation updated",
+    title: "Power Supply Systems",
     description:
-      "Instrumentation materials, PYQs, and solutions have been added.",
+      "Power Supply Systems materials, PYQs, and solutions have been added.",
     tag: "Live",
     date: "13-05-2026",
   },
   
   {
-    title: "Upcoming : Power Supply Systems",
+    title: "Upcoming : Field Theory",
     description:
-      "Power Supply Systems materials, PYQs, and solutions will be added soon.",
+      "Field Theory materials, PYQs, and solutions will be added soon.",
     tag: "Coming Soon",
     date: "soon",
   },
